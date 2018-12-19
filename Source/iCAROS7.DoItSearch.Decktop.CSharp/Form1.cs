392715797e8ce6cc639ebaafcb0a9031a959b9f3
iCAROS7 <hominlab@gmail.com>
2018-12-19 16:09:29 +0900

    Add Log4net & exception

--- Source/iCAROS7.DoItSearch.Decktop.CSharp/Form1.cs
+++ Source/iCAROS7.DoItSearch.Decktop.CSharp/Form1.cs
@@ -9,17 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Web;
 using Microsoft.VisualBasic;
+using System.Reflection;
 using System.IO;
 
 namespace iCAROS7.DoItSearch.Decktop.CSharp
 {
     public partial class Form1 : Form
     {
+        static readonly log4net.ILog Log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         Random rand = new Random();
         public int Cnt = 0;
         public int Max_Cnt = 10;
         public string[] keywords;
-        private DateTime startTime, startTime2;
+        private DateTime startTime;
         public Form1()
         {
             InitializeComponent();
@@ -40,6 +42,7 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Log.InfoFormat(@"프로그램 실행");
             int x = Screen.PrimaryScreen.Bounds.Width / 2 - this.Width / 2;
             int y = Screen.PrimaryScreen.Bounds.Height / 2 - this.Height / 2;
             this.Location = new Point(x, y);
@@ -51,16 +54,31 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
-            webBrowser1.Url = new Uri(@"https://twitter.com/search?q=" + HttpUtility.UrlEncode(keywords[Cnt]));
-            if (Cnt == keywords.Length -1)
+            try
             {
-                Cnt = 0;
+                webBrowser1.Url = new Uri(@"https://twitter.com/search?q=" + HttpUtility.UrlEncode(keywords[Cnt]));
+                if (Cnt == keywords.Length - 1)
+                {
+                    Cnt = 0;
+                }
+                else
+                {
+                    Cnt++;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Cnt++;
+                MessageBox.Show(@"" + ex);
             }
+            //webBrowser1.Url = new Uri(@"https://twitter.com/search?q=" + HttpUtility.UrlEncode(keywords[Cnt]));
+            //if (Cnt == keywords.Length - 1)
+            //{
+            //    Cnt = 0;
+            //}
+            //else
+            //{
+            //    Cnt++;
+            //}
         }
 
         private void startToolStripMenuItem_Click(object sender, EventArgs e)
@@ -70,6 +88,7 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
                 keywords = Keyword.Text.Split(',');
                 startTime = DateTime.Now;
                 label1.Text = @"검색 시간 : 00:00:00";
+                Log.InfoFormat(@"검색 시작 : {0}", Keyword.Text);
                 timer1.Enabled = true;
                 timer2.Enabled = true;
             }
@@ -84,17 +103,21 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
             timer1.Enabled = false;
             timer2.Enabled = false;
             Cnt = 0;
+            Log.InfoFormat(@"검색 중지");
             MessageBox.Show(@"중지 되었습니다!", @"안내", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void maxIntervalToolStripMenuItem_Click(object sender, EventArgs e)
         {
             string maxIntv = Microsoft.VisualBasic.Interaction.InputBox("검색을 할 사이에 잠깐 기다릴 최대 시간 초를 설정 합니다." + "\n" + "30을 입력하는 경우 5~30초 사이를 무작위로 기다립니다.", "검색 최대 간격 설정");
+
+            Log.InfoFormat(@"최대 검색 시간 변경 : {0}", maxIntv);
             try
             {
                 Max_Cnt = Int32.Parse(maxIntv);
                 if (Max_Cnt < 5)
                 {
+                    Log.ErrorFormat(@"최대 검색 시간 설정 오류 {0}초", maxIntv);
                     throw new FormatException();
                 }
             }
@@ -110,6 +133,7 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
             Settings.Instance.Keyword = Keyword.Text;
             Settings.Instance.LoadAtStart = loadAtStartToolStripMenuItem.Checked;
             Settings.Instance.Save();
+            Log.InfoFormat(@"설정 저장 완료");
             MessageBox.Show(@"저장되었습니다!", @"안내", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
@@ -118,6 +142,7 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
             Keyword.Text = Settings.Instance.Keyword;
             Max_Cnt = Settings.Instance.Max_Cnt;
             loadAtStartToolStripMenuItem.Checked = Settings.Instance.LoadAtStart;
+            Log.InfoFormat(@"설정 불러오기 완료");
             MessageBox.Show(@"불러왔습니다!", @"안내", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
@@ -133,6 +158,7 @@ namespace iCAROS7.DoItSearch.Decktop.CSharp
                 Settings.Instance.LoadAtStart = false;
                 saveToolStripMenuItem.PerformClick();
             }
+            Log.InfoFormat(@"시작시 설정 불러오기 상태 전환환");
         }
 
         private void timer2_Tick(object sender, EventArgs e)
